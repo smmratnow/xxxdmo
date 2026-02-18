8ede30c6b639d75641ad3cac3f56b84060126d67
Ekam <smmratnow@gmail.com>
2026-02-18 16:14:37 -0500

    Create all-Institution-data.js

---
+++ financial-institutions/all-Institution-data.js
@@ -0,0 +1,579 @@
+{
+  "allInstitutionNumbers": [
+    {
+      "financialInstitution": "Bank of Montreal",
+      "institutionNumber": 1,
+      "id": 2
+    },
+    {
+      "financialInstitution": "Scotiabank (The Bank of Nova Scotia)",
+      "institutionNumber": 2,
+      "id": 3
+    },
+    {
+      "financialInstitution": "Royal Bank of Canada",
+      "institutionNumber": 3,
+      "id": 4
+    },
+    {
+      "financialInstitution": "The Toronto-Dominion Bank",
+      "institutionNumber": 4,
+      "id": 5
+    },
+    {
+      "financialInstitution": "National Bank of Canada",
+      "institutionNumber": 6,
+      "id": 6
+    },
+    {
+      "financialInstitution": "Canadian Imperial Bank of Commerce",
+      "institutionNumber": 10,
+      "id": 7
+    },
+    {
+      "financialInstitution": "HSBC Bank Canada",
+      "institutionNumber": 16,
+      "id": 8
+    },
+    {
+      "financialInstitution": "Canadian Western Bank",
+      "institutionNumber": 30,
+      "id": 9
+    },
+    {
+      "financialInstitution": "Laurentian Bank of Canada",
+      "institutionNumber": 39,
+      "id": 10
+    },
+    {
+      "financialInstitution": "Bank of Canada",
+      "institutionNumber": 177,
+      "id": 11
+    },
+    {
+      "financialInstitution": "Alberta Treasury Branches",
+      "institutionNumber": 219,
+      "id": 12
+    },
+    {
+      "financialInstitution": "Royal Bank of Scotland N.V. (Canada Branch)",
+      "institutionNumber": 240,
+      "id": 13
+    },
+    {
+      "financialInstitution": "Bank of America, National Association",
+      "institutionNumber": 241,
+      "id": 14
+    },
+    {
+      "financialInstitution": "The Bank of New York Mellon",
+      "institutionNumber": 242,
+      "id": 15
+    },
+    {
+      "financialInstitution": "Bank of Tokyo-Mitsubishi UFJ (Canada)",
+      "institutionNumber": 245,
+      "id": 16
+    },
+    {
+      "financialInstitution": "BNP Paribas (Canada)",
+      "institutionNumber": 250,
+      "id": 17
+    },
+    {
+      "financialInstitution": "Citibank Canada",
+      "institutionNumber": 260,
+      "id": 18
+    },
+    {
+      "financialInstitution": "Deutsche Bank AG",
+      "institutionNumber": 265,
+      "id": 19
+    },
+    {
+      "financialInstitution": "Mega International Commercial Bank (Canada)",
+      "institutionNumber": 269,
+      "id": 20
+    },
+    {
+      "financialInstitution": "JPMorgan Chase Bank National Association",
+      "institutionNumber": 270,
+      "id": 21
+    },
+    {
+      "financialInstitution": "Korea Exchange Bank of Canada",
+      "institutionNumber": 275,
+      "id": 22
+    },
+    {
+      "financialInstitution": "Mizuho Corporate Bank Ltd. Canada Branch",
+      "institutionNumber": 277,
+      "id": 23
+    },
+    {
+      "financialInstitution": "UBS Bank (Canada)",
+      "institutionNumber": 290,
+      "id": 24
+    },
+    {
+      "financialInstitution": "Société Générale (Canada Branch)",
+      "institutionNumber": 292,
+      "id": 25
+    },
+    {
+      "financialInstitution": "State Bank of India (Canada) Alberta",
+      "institutionNumber": 294,
+      "id": 26
+    },
+    {
+      "financialInstitution": "Sumitomo Mitsui Banking Corporation of Canada",
+      "institutionNumber": 301,
+      "id": 27
+    },
+    {
+      "financialInstitution": "Amex Bank of Canada",
+      "institutionNumber": 303,
+      "id": 28
+    },
+    {
+      "financialInstitution": "Industrial and Commercial Bank of China (Canada)",
+      "institutionNumber": 307,
+      "id": 29
+    },
+    {
+      "financialInstitution": "Bank of China (Canada)",
+      "institutionNumber": 308,
+      "id": 30
+    },
+    {
+      "financialInstitution": "Citizens Bank of Canada",
+      "institutionNumber": 309,
+      "id": 31
+    },
+    {
+      "financialInstitution": "First Nations Bank of Canada",
+      "institutionNumber": 310,
+      "id": 32
+    },
+    {
+      "financialInstitution": "BofA Canada Bank",
+      "institutionNumber": 311,
+      "id": 33
+    },
+    {
+      "financialInstitution": "J.P. Morgan Bank Canada",
+      "institutionNumber": 314,
+      "id": 34
+    },
+    {
+      "financialInstitution": "CTC Bank of Canada",
+      "institutionNumber": 315,
+      "id": 35
+    },
+    {
+      "financialInstitution": "U.S. Bank National Association",
+      "institutionNumber": 318,
+      "id": 36
+    },
+    {
+      "financialInstitution": "Habib Canadian Bank",
+      "institutionNumber": 321,
+      "id": 37
+    },
+    {
+      "financialInstitution": "Rabobank Nederland",
+      "institutionNumber": 322,
+      "id": 38
+    },
+    {
+      "financialInstitution": "Capital One Bank (Canada Branch)",
+      "institutionNumber": 323,
+      "id": 39
+    },
+    {
+      "financialInstitution": "President’s Choice Financial",
+      "institutionNumber": 326,
+      "id": 40
+    },
+    {
+      "financialInstitution": "State Street",
+      "institutionNumber": 327,
+      "id": 41
+    },
+    {
+      "financialInstitution": "Citibank N.A.",
+      "institutionNumber": 328,
+      "id": 42
+    },
+    {
+      "financialInstitution": "Comerica Bank",
+      "institutionNumber": 330,
+      "id": 43
+    },
+    {
+      "financialInstitution": "First Commercial Bank",
+      "institutionNumber": 332,
+      "id": 44
+    },
+    {
+      "financialInstitution": "HSBC Bank USA National Association",
+      "institutionNumber": 333,
+      "id": 45
+    },
+    {
+      "financialInstitution": "Pacific & Western Bank of Canada",
+      "institutionNumber": 334,
+      "id": 46
+    },
+    {
+      "financialInstitution": "United Overseas Bank Limited",
+      "institutionNumber": 335,
+      "id": 47
+    },
+    {
+      "financialInstitution": "Maple Bank",
+      "institutionNumber": 336,
+      "id": 48
+    },
+    {
+      "financialInstitution": "Canadian Tire Bank",
+      "institutionNumber": 338,
+      "id": 49
+    },
+    {
+      "financialInstitution": "UBS AG Canada Branch",
+      "institutionNumber": 339,
+      "id": 50
+    },
+    {
+      "financialInstitution": "ICICI Bank Canada",
+      "institutionNumber": 340,
+      "id": 51
+    },
+    {
+      "financialInstitution": "Bank West",
+      "institutionNumber": 342,
+      "id": 52
+    },
+    {
+      "financialInstitution": "Dundee Bank of Canada",
+      "institutionNumber": 343,
+      "id": 53
+    },
+    {
+      "financialInstitution": "General Bank of Canada",
+      "institutionNumber": 344,
+      "id": 54
+    },
+    {
+      "financialInstitution": "Fifth Third Bank",
+      "institutionNumber": 345,
+      "id": 55
+    },
+    {
+      "financialInstitution": "Société Générale (Canada Branch) Ontario",
+      "institutionNumber": 346,
+      "id": 56
+    },
+    {
+      "financialInstitution": "Bridgewater Bank",
+      "institutionNumber": 347,
+      "id": 57
+    },
+    {
+      "financialInstitution": "The Northern Trust Company Canada Branch",
+      "institutionNumber": 349,
+      "id": 58
+    },
+    {
+      "financialInstitution": "DirectCash Bank",
+      "institutionNumber": 352,
+      "id": 59
+    },
+    {
+      "financialInstitution": "Jameson Bank",
+      "institutionNumber": 354,
+      "id": 60
+    },
+    {
+      "financialInstitution": "Shinhan Bank Canada",
+      "institutionNumber": 355,
+      "id": 61
+    },
+    {
+      "financialInstitution": "M&T Bank",
+      "institutionNumber": 357,
+      "id": 62
+    },
+    {
+      "financialInstitution": "HomEquity Bank",
+      "institutionNumber": 358,
+      "id": 63
+    },
+    {
+      "financialInstitution": "Walmart Canada Bank",
+      "institutionNumber": 359,
+      "id": 64
+    },
+    {
+      "financialInstitution": "Barclay’s Bank PLC Canada Branch",
+      "institutionNumber": 360,
+      "id": 65
+    },
+    {
+      "financialInstitution": "MonCana Bank of Canada",
+      "institutionNumber": 361,
+      "id": 66
+    },
+    {
+      "financialInstitution": "Community Trust Company",
+      "institutionNumber": 507,
+      "id": 67
+    },
+    {
+      "financialInstitution": "The Canada Trust Company",
+      "institutionNumber": 509,
+      "id": 68
+    },
+    {
+      "financialInstitution": "Laurentian Trust of Canada Inc.",
+      "institutionNumber": 522,
+      "id": 69
+    },
+    {
+      "financialInstitution": "Effort Trust Company",
+      "institutionNumber": 532,
+      "id": 70
+    },
+    {
+      "financialInstitution": "Investors Group Trust Co. Ltd.",
+      "institutionNumber": 536,
+      "id": 71
+    },
+    {
+      "financialInstitution": "Manulife Bank of Canada",
+      "institutionNumber": 540,
+      "id": 72
+    },
+    {
+      "financialInstitution": "CIBC Trust Corporation",
+      "institutionNumber": 548,
+      "id": 73
+    },
+    {
+      "financialInstitution": "Montreal Trust Company of Canada",
+      "institutionNumber": 550,
+      "id": 74
+    },
+    {
+      "financialInstitution": "Sun Life Financial Trust Inc.",
+      "institutionNumber": 551,
+      "id": 75
+    },
+    {
+      "financialInstitution": "Peace Hills Trust Company",
+      "institutionNumber": 568,
+      "id": 76
+    },
+    {
+      "financialInstitution": "Royal Trust Company",
+      "institutionNumber": 570,
+      "id": 77
+    },
+    {
+      "financialInstitution": "Royal Trust Corporation of Canada",
+      "institutionNumber": 580,
+      "id": 78
+    },
+    {
+      "financialInstitution": "National Trust Company",
+      "institutionNumber": 590,
+      "id": 79
+    },
+    {
+      "financialInstitution": "Royal Bank Mortgage Corporation",
+      "institutionNumber": 592,
+      "id": 80
+    },
+    {
+      "financialInstitution": "TD Mortgage Corporation",
+      "institutionNumber": 597,
+      "id": 81
+    },
+    {
+      "financialInstitution": "TD Pacific Mortgage Corporation",
+      "institutionNumber": 603,
+      "id": 82
+    },
+    {
+      "financialInstitution": "HSBC Mortgage Corporation (Canada)",
+      "institutionNumber": 604,
+      "id": 83
+    },
+    {
+      "financialInstitution": "Scotia Mortgage Corporation",
+      "institutionNumber": 606,
+      "id": 84
+    },
+    {
+      "financialInstitution": "CS Alterna Bank",
+      "institutionNumber": 608,
+      "id": 85
+    },
+    {
+      "financialInstitution": "Tangerine Bank",
+      "institutionNumber": 614,
+      "id": 86
+    },
+    {
+      "financialInstitution": "B2B Bank (formerly B2B Trust)",
+      "institutionNumber": 618,
+      "id": 87
+    },
+    {
+      "financialInstitution": "ResMor Trust Company",
+      "institutionNumber": 620,
+      "id": 88
+    },
+    {
+      "financialInstitution": "Peoples Trust Company",
+      "institutionNumber": 621,
+      "id": 89
+    },
+    {
+      "financialInstitution": "The Equitable Trust Company",
+      "institutionNumber": 623,
+      "id": 90
+    },
+    {
+      "financialInstitution": "Industrial Alliance Trust Inc.",
+      "institutionNumber": 625,
+      "id": 91
+    },
+    {
+      "financialInstitution": "Manulife Trust Company",
+      "institutionNumber": 626,
+      "id": 92
+    },
+    {
+      "financialInstitution": "Household Trust Company",
+      "institutionNumber": 630,
+      "id": 93
+    },
+    {
+      "financialInstitution": "Latvian Credit Union Limited",
+      "institutionNumber": 803,
+      "id": 94
+    },
+    {
+      "financialInstitution": "Communication Technologies Credit Union Limited",
+      "institutionNumber": 807,
+      "id": 95
+    },
+    {
+      "financialInstitution": "Arnstein Community Credit Union Limited",
+      "institutionNumber": 808,
+      "id": 96
+    },
+    {
+      "financialInstitution": "Central 1 Credit Union British Columbia",
+      "institutionNumber": 809,
+      "id": 97
+    },
+    {
+      "financialInstitution": "All Trans Financial Services Credit Union Limited",
+      "institutionNumber": 810,
+      "id": 98
+    },
+    {
+      "financialInstitution": "La Confédération des Caisses Populaires et D’Économie Desjardins du Québec",
+      "institutionNumber": 815,
+      "id": 99
+    },
+    {
+      "financialInstitution": "La Fédération des caisses populaires du Manitoba Inc.",
+      "institutionNumber": 819,
+      "id": 100
+    },
+    {
+      "financialInstitution": "Central 1 Credit Union Ontario",
+      "institutionNumber": 828,
+      "id": 101
+    },
+    {
+      "financialInstitution": "La Fédération des Caisses Populaires de l’Ontario Inc.",
+      "institutionNumber": 829,
+      "id": 102
+    },
+    {
+      "financialInstitution": "Airline Financial Credit Union Limited",
+      "institutionNumber": 830,
+      "id": 103
+    },
+    {
+      "financialInstitution": "Meridian Credit Union",
+      "institutionNumber": 837,
+      "id": 104
+    },
+    {
+      "financialInstitution": "Atlantic Central",
+      "institutionNumber": 839,
+      "id": 105
+    },
+    {
+      "financialInstitution": "Dundalk District Credit Union Limited",
+      "institutionNumber": 840,
+      "id": 106
+    },
+    {
+      "financialInstitution": "Alterna Savings and Credit Union",
+      "institutionNumber": 842,
+      "id": 107
+    },
+    {
+      "financialInstitution": "Goderich Community Credit Union Limited",
+      "institutionNumber": 844,
+      "id": 108
+    },
+    {
+      "financialInstitution": "Ontario Civil Service Credit Union Limited",
+      "institutionNumber": 846,
+      "id": 109
+    },
+    {
+      "financialInstitution": "Concentra Financial Services Association",
+      "institutionNumber": 853,
+      "id": 110
+    },
+    {
+      "financialInstitution": "Golden Horseshoe Credit Union Limited",
+      "institutionNumber": 854,
+      "id": 111
+    },
+    {
+      "financialInstitution": "La Fédération des Caisses Populaires Acadiennes Limitée",
+      "institutionNumber": 865,
+      "id": 112
+    },
+    {
+      "financialInstitution": "Credit Union Central of Manitoba Limited",
+      "institutionNumber": 879,
+      "id": 113
+    },
+    {
+      "financialInstitution": "Credit Union Central of Saskatchewan",
+      "institutionNumber": 889,
+      "id": 114
+    },
+    {
+      "financialInstitution": "Alliance des caisses populaires de l’Ontario Limitée",
+      "institutionNumber": 890,
+      "id": 115
+    },
+    {
+      "financialInstitution": "Credit Union Central Alberta Limited",
+      "institutionNumber": 899,
+      "id": 116
+    }
+  ]
+}
